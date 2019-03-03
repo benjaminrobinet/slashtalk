e3e5d326c71b6d2252b6f651b567a70ccdd018c6
Benjamin Robinet <benjamin.robinet@free.fr>
2019-03-03 22:34:18 +0100

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
       <b-row>
         <div class="w-25">
           <b-nav vertical>
-            <b-nav-item v-for="channel in channels">{{ channel }}</b-nav-item>
-            <b-nav-item>Create channel</b-nav-item>
+            <b-nav-item class="create" @click="showCreateChannelModal = !showCreateChannelModal"><font-awesome-icon icon="plus"/> Create channel</b-nav-item>
+            <b-nav-item v-for="channel in channels" v-b-tooltip.right :title="channel.topic">{{ channel.label }}</b-nav-item>
           </b-nav>
         </div>
         <div class="content w-75">
@@ -13,6 +13,29 @@
         </div>
       </b-row>
     </b-container>
+    <b-modal hide-footer title="Create new channel" v-model="showCreateChannelModal">
+      <b-form @submit.prevent="createChannel">
+        <b-form-group label="Label" label-for="label">
+          <b-form-input
+              id="label"
+              type="text"
+              v-model="form.label"
+              required
+              placeholder="Enter channel label" />
+        </b-form-group>
+
+        <b-form-group label="Topic" label-for="topic">
+          <b-form-input
+              id="topic"
+              type="text"
+              v-model="form.topic"
+              required
+              placeholder="Enter channel topic" />
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Create</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
@@ -25,15 +48,46 @@ export default {
   data(){
     return {
       channels: [],
-      showCreateChannelModal: false
+      showCreateChannelModal: false,
+      form: {
+        label: '',
+        topic: ''
+      }
     }
   },
   mounted(){
-    this.$axios.get('channels').then(resp => {
-      this.channels = resp.data;
-    }).catch(err => {
-      console.error(err);
-    });
+    this.getChannels();
+  },
+  methods:{
+    getChannels(){
+      this.$axios.get('channels').then(resp => {
+        this.channels = resp.data;
+      }).catch(err => {
+        console.error(err);
+      });
+    },
+    createChannel(){
+      this.$axios.post('channels', {
+        "label": this.form.label,
+        "topic": this.form.topic
+      }).then(resp => {
+        this.getChannels();
+        this.form.label = this.form.topic = '';
+        this.showCreateChannelModal = false;
+      }).catch(err => {
+        console.error(err);
+      })
+    }
   }
 }
 </script>
+
+<style scoped lang="scss">
+  .home{
+    .create{
+      a{
+        font-weight: bold;
+      }
+    }
+  }
+</style>
